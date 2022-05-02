6d9ae6a331b36f2013aade2c5297e7792a319f61
Fikerte-T <tofikertesfaye@gmail.com>
2022-05-02 18:33:29 +0300

    Add homepage and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "pages#index"
+  
+  resources :pages, only: %i[index]
+  resources :users do 
+    resources :categories do
+      resources :transactions
+    end
+  end
 end
