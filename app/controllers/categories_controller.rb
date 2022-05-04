23d4459ee34a82500d36f81c2858092d0734ad6c
Fikerte-T <tofikertesfaye@gmail.com>
2022-05-04 19:44:34 +0300

    New category page and style

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,29 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @user = current_user
+    @categories = Category.where(user_id: @user.id)
+  end
+
+  def new
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+    if @category.save
+        flash.notice = 'Category was successfully created.'
+        redirect_to user_categories_path(@category.current_user)
+      else
+        flash.alert = 'Category was not created.'
+        render :new
+      end
+  end
 
   def show; end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
+
 end
